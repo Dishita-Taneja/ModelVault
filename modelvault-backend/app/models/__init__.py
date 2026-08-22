4f0266677a93fbf33686d5c25937bd0ca73ffd2e
Dishita Taneja <dishitataneja@gmail.com>
2026-08-22 18:51:20 +0530

    Delete modelvault-backend directory

--- modelvault-backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.base import Base
-from app.models.user import User
-from app.models.model import MLModel
-from app.models.access_event import AccessEvent
-from app.models.anomaly_result import AnomalyResult
-
-__all__ = ["Base", "User", "MLModel", "AccessEvent", "AnomalyResult"]
